fe475a114a02f9d69089cba3c27dd69b866ec44d
Jeremy Lehman <jeremy.lehman1@angieslist.com>
2016-10-14 18:20:36 -0400

    add mongo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,28 @@
 
 var express = require('express');
-
+var Message = require('../models/Message');
 var router = express.Router();
 
 router.get('/', function(req, res) {
   res.send('Hey, I am the root route');
 });
 
-router.get('/message', function(req, res) {
-  res.send('You asked for a message, so now you have got one!');
+router.post('/messages', function(req, res, next) {
+  Message(req.body).save(function(err) {
+    if (err) {
+      res.send(err);
+    }
+    res.send('Message saved');
+  })
+});
+
+router.get('/messages', function(req, res, next) {
+  Message.find(function(err, messages) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(messages);
+  })
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 
+require('dotenv').config();
 var express = require('express');
 var routes = require('./app/routes');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', routes)
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+var connection = mongoose.connect(process.env.MONGODB).connection;
+connection.on('error', console.error.bind(console, 'connection error'));
+connection.once('open', function() {
+  console.log('MongoDB is now connected');
+});
+
+app.listen(process.env.PORT, function() {
+  console.log('Listening on port ' + process.env.PORT);
 });
 
 module.exports = app;
